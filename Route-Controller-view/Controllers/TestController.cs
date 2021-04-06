8a33b0556de28dff32e0b332e12fd9f3e7af5d91
smriad <skmdriad@gmail.com>
2021-04-06 17:52:09 +0800

    route-controller-view excercise

---
+++ Route-Controller-view/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestView() 
+        {
+            return View();
+        }
+
+        public string PrintMessage() 
+        {
+            return "<h1>Hello world</h1>";
+        }
+
+        public string Welcome(string name, int id=1)
+        {
+            return HttpUtility.HtmlEncode("Hello, " + name + " your ID is " + id);
+        }
+
+        public ActionResult ErrorMessage() 
+        {
+            return View();
+        }
+
+       
+    }
+}
