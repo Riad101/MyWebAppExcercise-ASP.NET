64bdc6e88e2eb7c616b267659516cd5fc91c0cb0
smriad <skmdriad@gmail.com>
2021-04-09 11:49:56 +0800

    set up sql connection'

--- Login_Form/Login_Form/Services/Data/SecurityDAO.cs
+++ Login_Form/Login_Form/Services/Data/SecurityDAO.cs
@@ -10,7 +10,7 @@ namespace Login_Form.Services.Data
     public class SecurityDAO
     {
         //connect to the database
-        string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         internal bool findByUser(userModel user)
         {
             //return (user.UserName =="Admin" && user.Password =="Security");
@@ -30,8 +30,32 @@ namespace Login_Form.Services.Data
                 //create the command and parameter objects
                 SqlCommand command = new SqlCommand(queryString, connection);
                 command.Parameters.Add("@UserName", System.Data.SqlDbType.VarChar, 50).Value = user.UserName;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                //open the database and run the command
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    reader.Close();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }                
 
             }
+            return success;
 
         }
     }
